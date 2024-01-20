205c4c695a619c12af482d281777b5f6f6c8ec63
Harsh Gupta <harsh.gupta@cognustechnology.com>
2024-01-20 16:22:47 +0530

    faq added

--- ipo/faq.js
+++ ipo/faq.js
@@ -141,6 +141,37 @@ const jsonData = {
       "question": "What is a buyback record date?",
       "answer": "The buyback record date is the date specified in the buyback offer on which shareholders must own the company's stock in their demat account to be eligible to participate in the buyback. Only those with the stock in their demat account on the record date can take part in the buyback offer."
     }
+  ],
+
+  "upcoming": [
+    {
+      "question": "What is an IPO? or What is IPO Meaning?",
+      "answer": "The IPO is an initial public offer where companies come up for their share sale to the public by offering their own privately held shares to the public. The companies should draft DRHP with SEBI for Initial Public Offer (IPO) to change their company identity from Private Limited to Limited. The company should file DRHP (draft red herring prospectus) and final RHP (red herring prospectus) for IPO. SEBI approves the Initial Public Offer for the companies, and then they go for the IPO. After RHP filed by the company decides the price band and the date to invest in the IPO via UPI or ASBA format for investors."
+    },
+    {
+      "question": "Which IPO is open today / this week? or Which IPO is coming soon?",
+      "answer": "The upcoming IPO this week is Medi Assist Healthcare, EPACK Durable, and Nova Agri Tech in January 2024."
+    },
+    {
+      "question": "Which are the best Upcoming IPO in 2024 in India?",
+      "answer": "The upcoming IPO in India in 2024 includes Ebixcash, Indiafirst Life, SPC Life Sciences, Tata Play, Lohia Corp, Nova Agritech, and more. The Upcoming IPO list might include a few more names after the market regulator SEBI’s approval."
+    },
+    {
+      "question": "Can I apply for IPO without a Demat account?",
+      "answer": "No, as per the SEBI rules, an individual needs a Demat Account to apply for an IPO."
+    },
+    {
+      "question": "Are IPOs a good investment?",
+      "answer": "Yes, an IPO is a good investment for the short term and the long term as well. For a company whose financial situation is good and the demand is high, investors should go for those IPOs."
+    },
+    {
+      "question": "How can one apply for upcoming IPOs online?",
+      "answer": "IPO investors can apply for the upcoming IPO via UPI-based online IPO applications or Bank through ASBA."
+    },
+    {
+      "question": "Where do I get an application form for an upcoming IPO?",
+      "answer": "Download blank ASBA IPO application forms from NSE or BSE website. You can get the IPO forms from the brokers as well."
+    }
   ]
 }
 
